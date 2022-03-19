7510a1c4b9d35e38d055d49d5077ba6e8abf2620
LopSided <40902730+Lpsd@users.noreply.github.com>
2022-03-19 07:35:36 +0000

    Remove old file

--- client/classes/Core.lua
+++
@@ -1,9 +0,0 @@
--- Author: Lpsd (https://github.com/Lpsd/redx)
--- See the LICENSE file @ root directory
-
-Core = inherit(Singleton)
-inherit(Autoloader, Core)
-
-function Core:constructor()
-    self:loadClasses()
-end
